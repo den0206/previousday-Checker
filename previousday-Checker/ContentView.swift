b8bab89801cfc6b09b49dac282e6e214d21ecefd
den0206 <mac.pop@mineo.jp>
2020-11-16 20:49:42 +0900

    Initial Commit

---
+++ previousday-Checker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  previousday-Checker
+//
+//  Created by 酒井ゆうき on 2020/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ previousday-Checker/previousday_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  previousday_CheckerApp.swift
+//  previousday-Checker
+//
+//  Created by 酒井ゆうき on 2020/11/16.
+//
+
+import SwiftUI
+
+@main
+struct previousday_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
